2c31cf9efef529e910ed1bed54c2126c3ab0ffa2
ChuckBombAK <snoqualmiechar@gmail.com>
2026-02-23 09:56:58 -0600

    Add robot speed variable and lab instructions
    Added robot speed variable and instructions for lab.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,9 +1,12 @@
 """
+robot_speed = 5 # m/s
+
 lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
+
 
 def main():
     print("Hello World!")
